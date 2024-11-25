b7d559b34a7b60a204e27bbac6da944a31dba338
joligon360 <joligon360@g.educaand.es>
2024-11-25 11:12:43 +0100

    commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
+import { ListaComponent } from './heroes/lista/lista.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { ContadorComponent } from './contador/contador.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [ ContadorComponent , HeroeComponent, ListaComponent
+  ]
 })
 export class AppComponent {
-  title = '02-bases';
+
+
 }
